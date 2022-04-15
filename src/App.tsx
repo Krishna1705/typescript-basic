592b57cbe2b52a6d9bd04e58368cb31baf9af380
Krishna1705 <ridpatel2907@gmail.com>
2022-04-15 22:20:42 +0200

    created new components for Todolist and SingleTodo and added code for its css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,110 +1,8 @@
 import React,{useState} from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import {Todo} from './model'
-//**************************************************************************************************************************** */
-let name:string;
-
-//if we want to give any type to the variable then we can use any or unknown as follow,buit unknown is a recommanded way than making use of any
-//let address:any;
-//let address:unknown;
-
-//if we want variable to contain number and string both of the types then we can do it with union as follows:
-let age:number | string;
-age=5;
-let isStudent:boolean;
-let hobbies:string[];
-let role:[number,string]; //is called a tuple
-
-//object declaration 
-//NOTE:HERE TYPE IS SOMETHING CALLED ALIAS:WHICH ARE OF TWO TYPES:
-//1)type 2)interface
-//>>>>>>>>>>>>>>>1)type is as follows
-type Person={
-    name:string,
-    age?:number //? ----is used to make property optional
-}
-let person : Person={
-      name:"krishna",
-      //age:30
-  }
-//other example of object ---which is basically one object extends another object's properties
-type X = {
-  a?:string,
-  b:number
-}
-//Extending a type via intersections
-type Y = X & {
-  c:string,
-  d:number
-}
-let y: Y={
- // a:"ppp",
-  b:6,
-  c:"kkk",
-  d:5
-}
-//A type cannot be changed after being created
-/* type Window = {
-  title: string
-}
-
-type Window = {
-  ts: number
-}
-NOTE: above cod ewill throw an error as follows
-Error: Duplicate identifier 'Window'.
- */
- 
-
-//>>>>>>>>>>>>>>2)interface is as follows:
-interface Person1{
-  name:string,
-  age?:number //? ----is used to make property optional
-}
-let person1: Person1={
-  name:"kajal",
-  age:3 //? ----is used to make property optional
-}
-
-//Extending an interface
-interface Animal {
-  name: string
-}
-
-interface Bear extends Animal {
-  honey: boolean
-}
-
-let bear: Bear={
-  name:"white bear",
-  honey:true
-}
-
-//Adding new fields to an existing interface
-interface Window {
-  title: string
-}
-
-interface Window {
-  ts: number
-}
-//array of an object declaration
-let lotsOfPeople: Person[]
-
-//******************************declaration of function in Typescript
-//1st way
-function printName1(name1:string){
-  //console.log("************************",name1)
-}
-printName1("kaju");
-
-//2nd way
-let printName2:(name2:string)=>void;//if function returns nothing then write void
-//here void returns undefined
-//instead of void we can use never also----which means that function never returns anything
-
-
 
 const App:React.FC=()=> {
   const [todo,setTodo]=useState<string>("");
@@ -115,6 +13,7 @@ const App:React.FC=()=> {
     e.preventDefault();
     if(todo){
       setTodos([...todos,{id:Date.now(),todo,isDone:false}])
+      setTodo("");
     }
   }
   console.log(">>>>>>>>>>",todos);
@@ -122,14 +21,7 @@ const App:React.FC=()=> {
     <div className="App">
        <span className='heading'> TASKIFY </span>
        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} /> 
-       {todos.map((i)=>(
-       <>
-       <li>
-       {i.todo}
-       </li>
-        
-       </>)
-       )}
+       <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './InputField.styles.css'
 
 interface Props{
@@ -11,12 +11,18 @@ interface Props{
 
 
 const InputField:React.FC<Props>=({todo,setTodo,handleAdd})=> {
-  
+  //use of useref() hook in typescript
+  const inputRef=useRef<HTMLInputElement>(null)
+
   return (
-    <form className='input' onSubmit={(e)=>handleAdd(e)}>
+    <form className='input' onSubmit={(e)=>{
+      handleAdd(e);
+      inputRef.current?.blur()
+      }}>
         <input type="input" placeholder='Enter Text here' className='input__box' 
         value={todo} 
         onChange={(e)=>setTodo(e.target.value)}
+        ref={inputRef}
         >
         </input>
         <button type="submit" className='input__submit'>Go</button>
